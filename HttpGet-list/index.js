652ea1421c4493606c0f7a56e38216b4f6b2cb31
Artturi Kakriainen <artturi.kakriainen@edu.turkuamk.fi>
2022-08-14 17:32:59 +0300

    added crud functions

---
+++ HttpGet-list/index.js
@@ -0,0 +1,10 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const triggerTask = { id: "Task" + Date.now(), descr: "Generic task" }
+
+    context.res = {
+        // status: 200,  /* Defaults to 200 */
+        body: JSON.stringify(triggerTask)
+    };
+}
