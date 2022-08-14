e6db3ff703ea3ac1a907ed40bd736569937011ae
Artturi Kakriainen <artturi.kakriainen@edu.turkuamk.fi>
2022-08-14 20:34:51 +0300

    init addded

--- HttpInit-db/index.js
+++ HttpInit-db/index.js
@@ -1,10 +1,25 @@
+const { MongoClient } = require("mongodb")
+const { v4: uuidv4 } = require("uuid")
+
+const url = "mongodb://pilvi-app-mongodb:qafTav6Deh4Ltz3f4tWHSDtNg5XUH6BqhnicV6kcWjRruDl6TIGMnBRWjCnbu0YFSnpeL8HnDe8o4iHQOan2MA==@pilvi-app-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@pilvi-app-mongodb@"
+const client = new MongoClient(url)
+
+
+let task = {
+    _id: uuidv4(),
+    task: "kaljaa"
+}
+
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const triggerTask = { id: "Task" + Date.now(), descr: "Generic task" }
-
+    await client.connect();
+    const database = client.db("PilviTasks")
+    const collection = database.collection("Tasks")
+    await collection.deleteMany({})
+    await collection.insertOne({ task })
     context.res = {
         // status: 200,  /* Defaults to 200 */
-        body: JSON.stringify(triggerTask)
+        body: "init is done"
     };
 }
