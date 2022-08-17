964add12c39ce804e538ada964a7e99e19c078f2
Artturi Kakriainen <artturi.kakriainen@edu.turkuamk.fi>
2022-08-17 13:46:58 +0300

    db conn string key

--- HttpPost-item/index.js
+++ HttpPost-item/index.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require("mongodb")
 
-const url = "mongodb://pilvi-app-mongodb:qafTav6Deh4Ltz3f4tWHSDtNg5XUH6BqhnicV6kcWjRruDl6TIGMnBRWjCnbu0YFSnpeL8HnDe8o4iHQOan2MA==@pilvi-app-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@pilvi-app-mongodb@"
+var connectionString = Environment.GetEnvironmentVariable("TaskDbConnectionString");
+
+const url = connectionString
 const client = new MongoClient(url)
 
 
